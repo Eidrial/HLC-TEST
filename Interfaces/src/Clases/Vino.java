0c000a6c4eefabc83b2154ff4e389d5164e95e2c
Azahara <mariaazahara.parrales-cuevas@iesruizgijon.com>
2024-10-08 13:29:53 +0200

    Merge branch 'master' of https://github.com/Eidrial/HLC-TEST.git

---
+++ Interfaces/src/Clases/Vino.java
@@ -0,0 +1,20 @@
+package Clases;
+
+public class Vino {
+	
+	//Atributos
+	private String marca;
+	private String tipoVino;
+	private double grados;
+	private double precio;
+	
+	//Constructor
+	public Vino(String marca, String tipoVino, double grados, double precio) {
+		this.marca = marca;
+		this.tipoVino = tipoVino;
+		this.grados = grados;
+		this.precio = precio;
+	}
+	
+	
+}
